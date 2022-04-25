fd183fb2b4c0ad53e05ef38bce4c91e539e06808
SebastienLludrigas <sebastienlludrigas@gmail.com>
2022-04-25 08:33:36 +0200

    S1 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CocktailDetailsComponent,
+    CocktailListComponent
   ],
   imports: [
     BrowserModule
